a6faa8549bead7f69490fc637a4c03cb0a27ef89
Gabriel Lucas <gabriellucaslc10@gmail.com>
2025-04-30 19:24:55 -0300

    Importando imagem e criando react e css da logo

---
+++ src/Components/CapysLogo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CapysLogo.css';
+
+const CapysLogo = () => {
+  return (
+    <div className="capys-logo">
+      <img src="src/assets/CapysLogo.png" alt="Capys Logo" />
+    </div>
+  );
+};
+
+export default CapysLogo;
